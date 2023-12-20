d270084aee5a42ce78d865c270d588e805ae5314
Sergey Antipov <forvee@yandex.ru>
2023-12-20 17:51:21 +0300

    Mesto. Базовая функциональность

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-// @todo: Темплейт карточки
+  // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
@@ -7,19 +7,30 @@ const cardsContainer = container.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
-// @todo: Функция удаления карточки
-function cardDelete () {
-    const card = document.querySelector('.card');
-    card.remove();
+function createCard (element) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__image').src = element.link;
+    cardElement.querySelector('.card__image').alt = element.name;
+    cardElement.querySelector('.card__title').textContent = element.name;
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener("click", deleteCard);
+    return cardElement
 }
+
+// @todo: Функция удаления карточки
+function deleteCard(evt) {
+    const eventTarget = evt.target;
+    eventTarget.closest('.card').remove();
+  }
 
 // @todo: Вывести карточки на страницу
 
-initialCards.forEach(function(element) {
-    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-    cardElement.querySelector('.card__image').src = element.link;
-    cardElement.querySelector('.card__title').textContent = element.name;
-    cardsContainer.append(cardElement);
-    const deleteButton = cardElement.querySelector('.card__delete-button');
-    deleteButton.addEventListener('click', cardDelete);
-});
+function renderCards() {
+    initialCards.forEach(function(element) {
+    const card = createCard(element);
+    cardsContainer.prepend(card);
+  });
+}
+
+renderCards();
+
