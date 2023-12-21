e278e334de6d0eac38cdb46206735805375c295f
Sergey Antipov <forvee@yandex.ru>
2023-12-21 19:31:54 +0300

    Mesto. Базовая функциональность

--- scripts/index.js
+++ scripts/index.js
@@ -7,14 +7,14 @@ const cardsContainer = container.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
-function createCard (element) {
+function createCard (element, deleteCard) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
     cardElement.querySelector('.card__image').src = element.link;
     cardElement.querySelector('.card__image').alt = element.name;
     cardElement.querySelector('.card__title').textContent = element.name;
     const deleteButton = cardElement.querySelector('.card__delete-button');
-    deleteButton.addEventListener("click", deleteCard);
-    return cardElement
+    deleteButton.addEventListener('click', deleteCard);
+    return cardElement;
 }
 
 // @todo: Функция удаления карточки
@@ -27,10 +27,9 @@ function deleteCard(evt) {
 
 function renderCards() {
     initialCards.forEach(function(element) {
-    const card = createCard(element);
+    const card = createCard(element, deleteCard);
     cardsContainer.prepend(card);
   });
 }
 
 renderCards();
-
